585bd0e542acea7833ffb1999e4d898dc3815161
shuvo247 <mdshakibul.shuvo@gmail.com>
2023-03-22 23:52:34 +0600

    Blank rection plugin

---
+++ reaction-button.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Plugin Name: Reaction Button
+ * Description: A wordpress plugin to accept react from user
+ * Plugin URI: https://reaction-button.com
+ * Author: MD Shakibul Islam
+ * Author URI: https://github.com/shuvo247/
+ * Version: 1.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
